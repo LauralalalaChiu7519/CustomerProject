ef8334ea76547c627dde67fe05f77a5244b166d3
laurachiu <laurachiu@liontravel.com>
2020-10-31 01:05:34 +0800

    客戶名稱、聯絡人數量、銀行帳戶數量

---
+++ CustomerProject/Controllers/vwCustomerReportsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CustomerProject.Models;
+
+namespace CustomerProject.Controllers
+{
+    public class vwCustomerReportsController : Controller
+    {
+        private CustomerDataEntities db = new CustomerDataEntities();
+
+        // GET: vwCustomerReports
+        public ActionResult Index()
+        {
+            return View(db.vwCustomerReport.ToList());
+        }
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
